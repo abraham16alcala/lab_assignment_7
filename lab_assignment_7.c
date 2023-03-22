30a06edc69b790b063e1d8d886b9b897b8999966
abraham16alcala <125482872+abraham16alcala@users.noreply.github.com>
2023-03-22 14:33:50 -0400

    Update lab_assignment_7.c

--- lab_assignment_7.c
+++ lab_assignment_7.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
 
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void bubbleSort(int arr[], int n)
+{
+    int i, j, numSwaps;
+
+    for (i = 0; i < n-1; i++)
+    {
+        numSwaps = 0;
+
+        for (j = 0; j < n-i-1; j++)
+        {
+            if (arr[j] > arr[j+1])
+            {
+                swap(&arr[j], &arr[j+1]);
+                numSwaps++;
+            }
+        }
+
+        printf("Pass #%d: %d\n", i+1, numSwaps);
+    }
+}
+
+int main()
+{
+    int arr[] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
+    bubbleSort(arr, 9);
+    return 0;
+}
